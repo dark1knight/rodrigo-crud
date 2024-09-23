7746f21e83945f2353c1dfff24adad157805e684
dark1knight <nakamuta26@gmail.com>
2024-09-23 11:58:13 -0300

    inicialização

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+// src/App.tsx
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import axios from 'axios';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface SearchResult {
+  id: number;
+  name: string;
+  email: string;
 }
 
+const App: React.FC = () => {
+  const [results, setResults] = useState<SearchResult[]>([]);
+
+  const handleSearch = async (query: string) => {
+    try {
+      const response = await axios.get<SearchResult[]>(`https://localhost:7006/api/Users/search?query=${query}`);
+      setResults(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <div className='search-bar-background'>
+        <SearchBar onSearch={handleSearch} />
+      </div>
+      <div className="container">
+        <ul>
+          {results.map((user) => (
+            <li key={user.id}>
+              <p><strong>Name:</strong> {user.name}</p>
+              <p><strong>Email:</strong> {user.email}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default App;
+
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+// src/components/SearchBar.tsx
+import React, { useState } from 'react';
+import './SearchBar.css';
+import { FaSearch } from 'react-icons/fa';
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState<string>('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+      <div className="search-bar-container">
+        <input
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          placeholder="Search..."
+          className="search-input"
+        />
+        <button className="search-button" onClick={handleSearch}>
+          <FaSearch />
+        </button>
+    </div>
+  );
+};
+
+export default SearchBar;
